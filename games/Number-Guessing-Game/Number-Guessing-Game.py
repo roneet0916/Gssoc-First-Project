bb76291dde0669856bf15cea59b647b7aae78769
Roneet sarode <saroderoneet@gmail.com>
2026-05-13 12:43:19 +0530

    gerr

--- games/Number-Guessing-Game/Number-Guessing-Game.py
+++ games/Number-Guessing-Game/Number-Guessing-Game.py
@@ -7,16 +7,19 @@ print("🎮 Welcome to the Number Guessing Game! 🎮")
 print("I'm thinking of a number between 1 and 100...\n")
 
 while True:
-    num = int(input("🤔 Guess the Number (1 - 100): "))
+    try:
+        num = int(input("🤔 Guess the Number (1 - 100): "))
 
-    if (num >= 1) and (num <= 100):
-        if num > num1:
-            print("📈 Too High!! Try lower...\n")
-        elif num < num1:
-            print("📉 Too Low!! Try higher...\n")
-        elif num == num1:
-            print("🎉🎊 BINGO! You guessed the correct number! 🎊🎉")
-            print(f"✨ The number was {num1} ✨")
-            break
-    else:
-        print("❌ INVALID INPUT!! Please enter a number between 1 and 100.\n")
+        if (num >= 1) and (num <= 100):
+            if num > num1:
+                print("📈 Too High!! Try lower...\n")
+            elif num < num1:
+                print("📉 Too Low!! Try higher...\n")
+            elif num == num1:
+                print("🎉🎊 BINGO! You guessed the correct number! 🎊🎉")
+                print(f"✨ The number was {num1} ✨")
+                break
+        else:
+            print("❌ INVALID INPUT!! Please enter a number between 1 and 100.\n")
+    except ValueError:
+        print("❌ INVALID INPUT!! Please enter a valid number between 1 and 100.\n")
--- math/Fibonacci-Series/Fibonacci-Series.py
+++ math/Fibonacci-Series/Fibonacci-Series.py
@@ -3,19 +3,18 @@
 print("🔢 Fibonacci Series Generator 🔢")
 print("📐 Pattern: 0, 1, 1, 2, 3, 5, 8, 13...\n")
 
-series = [0, 1, 1]
 n = int(input("➡️  Enter number of terms: "))
 
-num1 = 0
-num2 = 1
-num3 = num2
-i = 1
-
-while i <= n:
-    i += 1
-    num1, num2 = num2, num3
-    num3 = num1 + num2
-    series.append(num3)
+if n <= 0:
+    print("❌ Please enter a positive number!")
+elif n == 1:
+    series = [0]
+elif n == 2:
+    series = [0, 1]
+else:
+    series = [0, 1]
+    for i in range(2, n):
+        series.append(series[i-1] + series[i-2])
 
 print("\n✨ Fibonacci Series:")
 print(" → ".join(map(str, series)))
